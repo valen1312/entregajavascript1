785c5998420c4fd644f88c622b62299d1254031b
valenc <divisualstudio1@gmail.com>
2023-03-15 20:39:41 -0300

    Cambios de array

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-// Pidiendo al usuario que elija su sector
-let sector = prompt("Elija su sector:\n1-Industrial\n2-Financiera\n3-Servicios");
+// Función para obtener el estándar recomendado según la combinación de sector y tamaño
+function obtenerEstándar(sector, tamaño) {
+  const combinacion = sector + tamaño;
 
-// Pidiendo al usuario que elija su tamaño
-let tamaño = prompt("Elija su tamaño:\na-1-100\nb-100-300\nc-300-1000\nd-1000 o más");
-
-// Concatenando las respuestas del usuario para utilizar en el switch
-let combinacion = sector + tamaño;
-
-// Utilizando un switch para determinar la acción correspondiente a la combinación de respuestas
-switch (combinacion) {
-  case "1a":
-  case "1b":
-  case "1c":
-  alert ("El estandar recomendado es NIST");
-    break;
-case "2a":
-case "2b":
-case "2c":
- alert ("Se recomienda el estandar NIST acompañado de un SGSI basado en ISO27001")
+  switch (combinacion) {
+    case "1a":
+    case "1b":
+    case "1c":
+      return "NIST";
+    case "2a":
+    case "2b":
+    case "2c":
+      return "NIST acompañado de un SGSI basado en ISO27001";
+    default:
+      return "Estándar no disponible para esta combinación de sector y tamaño.";
+  }
 }
 
+// Pidiendo al usuario que elija su sector y tamaño
+const sector = prompt("Elija su sector:\n1-Industrial\n2-Financiera\n3-Servicios");
+const tamaño = prompt("Elija su tamaño:\na-1-100\nb-100-300\nc-300-1000\nd-1000 o más");
 
-let numControles = parseInt(prompt("Ingrese la cantidad de controles que desea agregar: "));
-let controles = [];
+// Obteniendo el estándar recomendado para la combinación de sector y tamaño
+const estándarRecomendado = obtenerEstándar(sector, tamaño);
+alert(`El estándar recomendado es ${estándarRecomendado}.`);
+
+// Pidiendo al usuario que ingrese los controles que desea agregar
+const numControles = parseInt(prompt("Ingrese la cantidad de controles que desea agregar: "));
+const controles = [];
 
 for (let i = 0; i < numControles; i++) {
-  let control = prompt(`Ingrese el nombre del control ${i+1}: `);
+  const control = prompt(`Ingrese el nombre del control ${i+1}: `);
   controles.push(control);
 }
 
-let controlesStr = controles.join(", ");
+// Filtrando los controles que contienen la palabra "seguridad"
+const controlesSeguridad = controles.filter(control => control.toLowerCase().includes("seguridad"));
 
-alert(`La lista de controles ingresados es: ${controlesStr}`);
+// Imprimiendo la lista de controles ingresados y los controles relacionados con seguridad
+const controlesStr = controles.join(", ");
+const controlesSeguridadStr = controlesSeguridad.join(", ");
+alert(`La lista de controles ingresados es: ${controlesStr}. Los controles relacionados con seguridad son: ${controlesSeguridadStr}.`);
 
+// Buscando un control específico ingresado por el usuario
+const controlBuscado = prompt("Ingrese el nombre de un control para buscarlo en la lista:");
+const controlEncontrado = controles.find(control => control.toLowerCase() === controlBuscado.toLowerCase());
+
+if (controlEncontrado) {
+  alert(`El control "${controlEncontrado}" ha sido encontrado en la lista.`);
+} else {
+  alert(`El control "${controlBuscado}" no ha sido encontrado en la lista.`);
+}
